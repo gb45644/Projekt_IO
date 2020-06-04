867778fa893dbea5bd1c1c6d48288d8484fc0f76
L <lukaszturowski0@gmail.com>
2020-06-04 15:44:13 +0200

    dodano port do sql

--- LoadData.py
+++ LoadData.py
@@ -1,6 +1,7 @@
 import pymysql
 
 db = pymysql.connect(host='localhost',
+                     port=3306,
                      user='root',
                      password='root',
                      database='io')
