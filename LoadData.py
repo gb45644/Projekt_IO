eb67d6f3587b8ba9a7625bcf2628454570900797
L <lukaszturowski0@gmail.com>
2020-06-06 16:44:22 +0200

    Merge branch 'master' of F:\GitHub\Projekt_IO with conflicts.

--- LoadData.py
+++ LoadData.py
@@ -9,29 +9,28 @@ def sql(login, haslo):
     fetch_queries = 'Select * from credentials WHERE login LIKE %s;'
 
     cursor.execute(fetch_queries, login)
-    lines=cursor.fetchall()
+    lines = cursor.fetchall()
     for lines in lines:
         if lines[1] == login and lines[2] == haslo:
             flaga = lines[3]
             return flaga
 
-def grafik(nazwisko):
+
+def grafik():
     db = pymysql.connect('localhost', 'root', 'root', 'io')
     cursor = db.cursor()
 
     fetch_queries = 'Select emploee_list.id,  emploee_list.imie,  emploee_list.nazwisko, calendar.id, calendar.year, ' \
                     'calendar.month, calendar.day, calendar.work From emploee_list Join graphic ON emploee_list.id = ' \
-                    'graphic.emploee_fk Join calendar ON calendar.id = graphic.calendar_fk  WHERE  nazwisko LIKE %s'
+                    'graphic.emploee_fk Join calendar ON calendar.id = graphic.calendar_fk  '
 
-
-    cursor.execute(fetch_queries,nazwisko)
-    lines=cursor.fetchall()
+    cursor.execute(fetch_queries)
+    lines = cursor.fetchall()
     for lines in lines:
         print(lines)
 
 
     db.commit()
     db.close()
-
 
 grafik()
